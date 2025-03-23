38a97763d1866fbbe76b20474ac2dcc30b659722
Yahwthani <0246038@up.edu.mx>
2025-03-22 20:02:53 -0600

    First two problems

---
+++ d3Lab/Exercise/01_D3_Fundamentals/js/main.js
@@ -0,0 +1,53 @@
+var svg = d3.select("#chart-area").append("svg")
+	.attr("width", 400)
+	.attr("height", 400);
+
+var circle = svg.append("circle")
+	.attr("cx", 130)
+	.attr("cy", 180)
+	.attr("r", 50)
+	.attr("fill", "blue"); 
+    
+var rect_1 = svg.append("rect")
+	.attr("x", 80)
+	.attr("y", 180)
+	.attr("width", 100)
+	.attr("height", 100)
+	.attr("fill","red");
+
+var rect_2 = svg.append("rect")
+	.attr("x", 120)
+	.attr("y", 235)
+	.attr("width", 20)
+	.attr("height", 45)
+	.attr("fill","brown");
+
+var rect_3 = svg.append("rect")
+	.attr("x", 90)
+	.attr("y", 200)
+	.attr("width", 10)
+	.attr("height", 10)
+	.attr("fill","lightblue");
+
+var rect_3 = svg.append("rect")
+	.attr("x", 90)
+	.attr("y", 215)
+	.attr("width", 10)
+	.attr("height", 10)
+	.attr("fill","lightblue");
+
+var rect_3 = svg.append("rect")
+	.attr("x", 105)
+	.attr("y", 200)
+	.attr("width", 10)
+	.attr("height", 10)
+	.attr("fill","lightblue");
+
+var rect_3 = svg.append("rect")
+	.attr("x", 105)
+	.attr("y", 215)
+	.attr("width", 10)
+	.attr("height", 10)
+	.attr("fill","lightblue");
+
+
